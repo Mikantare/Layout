4bc171363721f92a074ba99caf5c4928c7107a7b
Mikantare <abespalov33@gmail.com>
2021-12-04 14:40:13 +0300

    11

---
+++ app/src/main/java/com/bespalov/layout/SplachScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.bespalov.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplachScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splach_screen)
+    }
+}
